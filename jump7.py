94e6bb9b84add6ec475ec9636f543d61745c1fb5
zsn <z717@qq.com>
2019-10-07 18:42:13 +0800

    jump7

---
+++ jump7.py
@@ -0,0 +1,9 @@
+for i in range(1,101):
+        if i % 7 == 0:
+                    continue
+        elif i % 10 ==7:
+                    continue
+        elif i // 10 == 7:
+                    continue
+        else:
+                    print(i)
